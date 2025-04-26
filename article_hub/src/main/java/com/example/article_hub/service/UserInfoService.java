e6a644f4a494d8024dad9ac752ce91d88cc77f2a
Deepak Hudge <100028762+Hudgedeepak@users.noreply.github.com>
2025-04-26 23:54:30 +0530

    Added UserInfoService interface to define user-related service methods like add, login, update, and check token

---
+++ article_hub/src/main/java/com/example/article_hub/service/UserInfoService.java
@@ -0,0 +1,23 @@
+package com.example.article_hub.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.example.article_hub.entity.AuthRequest;
+import com.example.article_hub.entity.UserInfo;
+
+public interface UserInfoService {
+
+	
+	ResponseEntity<?> addNewAppuser(UserInfo userinfo);
+
+	ResponseEntity<?> login(AuthRequest authRequest);
+
+	ResponseEntity<?> getAllAppuser();
+
+	ResponseEntity<?> updateUserStatus(UserInfo userInfo);
+
+	ResponseEntity<?> checkToken();
+
+	ResponseEntity<?> updateUser(UserInfo userInfo);
+
+}
