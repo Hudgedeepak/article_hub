d4216436d50d20cc9b3ccc0bbdf6c51f9dbff3e2
Deepak Hudge <100028762+Hudgedeepak@users.noreply.github.com>
2025-05-17 16:22:24 +0530

    some changes

--- frontend_Art/frontEnd/src/app/app.module.ts
+++ frontend_Art/frontEnd/src/app/app.module.ts
@@ -8,11 +8,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './shared/material-module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgxUiLoaderConfig, NgxUiLoaderModule, PB_DIRECTION, SPINNER } from 'ngx-ui-loader';
 import { HomeComponent } from '../app/home/home.component';
 import { LoginComponent } from '../app/login/login.component';
-import { ConfirmationComponent } from './dialog/confirmation/confirmation.component';
+import { ConfirmationComponent } from './admin/dialog/confirmation/confirmation.component';
+import { TokenInterceptor } from './services/token.interceptor';
+import { QuillModule } from 'ngx-quill';
+import { SharedModule } from './shared/shared.module';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserRegisterPageComponent } from './user-register-page/user-register-page.component';
+
+
 
 const ngxUiLoaderConfig:NgxUiLoaderConfig={
   text:"Loading...",
@@ -34,7 +42,9 @@ const ngxUiLoaderConfig:NgxUiLoaderConfig={
     AppComponent,
     HomeComponent,
     LoginComponent,
-    ConfirmationComponent
+    UserRegisterComponent,
+    ArticleDetailsComponent,
+    UserRegisterPageComponent
   ],
   imports: [
     BrowserModule,
@@ -45,9 +55,11 @@ const ngxUiLoaderConfig:NgxUiLoaderConfig={
     FlexLayoutModule ,
     MaterialModule,
     HttpClientModule,
-    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
+    QuillModule.forRoot(),
+    SharedModule
   ],
-  providers: [],
+  providers: [HttpClientModule,{provide:HTTP_INTERCEPTORS, useClass:TokenInterceptor, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
